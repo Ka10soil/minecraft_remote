e2614e8a65402235e4e4dc5930709b7414de0c6c
ka10soil <86522732+ka10soil@users.noreply.github.com>
2022-05-29 15:44:13 +0900

    demo3.pyにピラミッド作成プログラムを追加

---
+++ demo3.py
@@ -0,0 +1,13 @@
+# Hello World for Minecraft Java Edition 1.16.5
+# mcje, MCJE: Minecraft Java Edition
+from mcje.minecraft import Minecraft
+import param_MCJE as param
+
+mc = Minecraft.create(port=param.PORT_MC)
+
+size = 5
+x = -14
+y = 63
+z = 14
+for num in range(size):
+    mc.setBlocks(x+size-1-num, y+num, z+size-1-num, x-size+1+num, y+num, z-size+1+num, param.AIR)
